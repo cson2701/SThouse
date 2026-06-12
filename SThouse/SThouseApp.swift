735e04e592ae89e71de96230fc95463ca3e16f4c
Gavin Song <gavin.song@gumtreegroup.com.au>
2026-06-13 09:40:15 +1000

    Initial Commit

---
+++ SThouse/SThouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SThouseApp.swift
+//  SThouse
+//
+//  Created by Gavin Song on 13/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct SThouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
